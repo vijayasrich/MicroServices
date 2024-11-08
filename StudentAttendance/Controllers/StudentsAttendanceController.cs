8f4d815b61ddb1ba1de03d56ddce2c37130d50a8
VIJJI <vijayasrichakiri@gmail.com>
2024-11-08 12:56:37 +0530

    Add project files.

---
+++ StudentAttendance/Controllers/StudentsAttendanceController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentAttendance.Models;
+
+namespace StudentAttendance.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsAttendanceController : ControllerBase
+    {
+        static List<StuAttendance> attendanceList;
+        [HttpGet]
+        public List<StuAttendance> Get()
+        {
+            attendanceList = new List<StuAttendance> {
+               new StuAttendance() { StudentID=101,StudentName="Aara", AttendancePercentage=90},
+                new StuAttendance() { StudentID=102,StudentName="luna", AttendancePercentage=91},
+                new StuAttendance() { StudentID=103,StudentName="Sara", AttendancePercentage=92},
+                new StuAttendance() { StudentID=104,StudentName="Tara", AttendancePercentage=93},
+
+          };
+            return attendanceList;
+        }
+    }
+}
---
+++ StudentAttendance/Models/StuAttendance.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentAttendance.Models
+{
+    public class StuAttendance
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public double AttendancePercentage { get; set; }
+    }
+}
